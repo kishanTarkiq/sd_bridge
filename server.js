9c8434fec37f027a8e353655360c6690df26e7cb
kishanTarkiq <145452780+kishanTarkiq@users.noreply.github.com>
2023-09-19 17:26:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,59 +3,28 @@ var bodyParser = require('body-parser')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
-})
-
-var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
+//var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
 
 app.get('/messages', (req, res) => {
 io.emit('message', req.body);
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
+  
 })
 
 
 app.get('/messages/:user', (req, res) => {
 io.emit('message', req.body);
-  var user = req.params.user
-  Message.find({name: user},(err, messages)=> {
-    res.send(messages);
-  })
+  
 })
 
 
 app.post('/messages', async (req, res) => {
 io.emit('message', req.body);
-  try{
-    var message = new Message(req.body);
-
-    var savedMessage = await message.save()
-      console.log('saved');
-
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-      res.sendStatus(200);
-  }
-  catch (error){
-    res.sendStatus(500);
-    return console.log('error',error);
-  }
-  finally{
-    console.log('Message Posted')
-  }
-
+  
 })
 
 
